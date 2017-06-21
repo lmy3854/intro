96e7da8c990ec74ccd48d1e47fed32ae368c32ec
ted <ted@listensoft.net>
2017-06-21 17:31:36 +0900

    validate 수정

--- js/index.js
+++ js/index.js
@@ -1,88 +1,82 @@
-/**
-			 * Author: Heather Corey
-			 * jQuery Simple Parallax Plugin
-			 **/
-			(function($) {
-				$.fn.parallax = function(options) {
-					var windowHeight = $(window).height();
+		var msg={
+			 emptyName:"ユーザーネームを入力してください"
+			,minName:"ユーザーネームは〇文字以上入力してください"
+			,emptyMail:"メールアドレスを入力してください"
+			,wrongMail:"メールアドレスは半角英数字で入力してください"
+			,success:"送信完了"
+			,error:"正しく入力されていない項目があります。もう一度ご入力ください。"
+		}
 
-					// Establish default settings
-					var settings = $.extend({
-							speed: 0.15
-					}, options);
+		$(document).ready(function() {
 
-					// Iterate over each object in collection
-					return this.each( function() {
-						// Save a reference to the element
-						var $this = $(this);
-						// Set up Scroll Handler
-						$(document).scroll(function(){
-							var scrollTop = $(window).scrollTop();
-							var offset = $this.offset().top;
-							var height = $this.outerHeight();
-
-							// Check if above or below viewport
-							if (offset + height <= scrollTop || offset >= scrollTop + windowHeight) {
-								return;
-							}
-
-							var yBgPosition = Math.round((offset - scrollTop) * settings.speed);
-
-							 // Apply the Y Background Position to Set the Parallax Effect
-							$this.css('background-position', 'center ' + yBgPosition + 'px');
-						});
-					});
-				}
-			}(jQuery));
-
-			$('.content1').parallax({speed : 0.3});
-			$('.content2').parallax({speed : 0.3});
-			$('.content2-1').parallax({speed : 0.3});
-			$('.content2-2').parallax({speed : 0.3});
-			$('.content2-3').parallax({speed : 0.3});
-			$('.content3-1').parallax({speed : 0.3});
-			$('.content3-2').parallax({speed : 0.3});
-			$('.content4').parallax({speed : 0.3});
-			$('.content5').parallax({speed : 0.3});
-
-			var msg={
-				 noName:"※は必須入力です"
-				,noMail:"※は必須入力です"
-				,success:"送信完了"
-				,error:"正しく入力されていない項目があります。もう一度ご入力ください。"
-			}
-
-			$(document).ready(function(){
-				$("#saveBtn").on("click",function(){
-					if($.trim($("#name").val()) == ""){
-						$("div#modalMsg").text(msg.noName);
-						$("#bs-example-modal-lg").modal("toggle");
-						return false;
-					}else if($.trim($("#mail").val()) == ""){
-						$("div#modalMsg").text(msg.noMail);
-						$("#bs-example-modal-lg").modal("toggle");
-						return false;
-					}
-					$.ajax({
-						 url:"https://script.google.com/macros/s/AKfycbwow4e86Vh8Syq_1fzpct7STCHHpnFkU2etQUVhBE9wN9oqtpE/exec"
-						,data:{name:$("#name").val()
-							, mail:$("#mail").val()
-							, gender:$("input[name='gender']:checked").val()
-							, age:$("select#age option:selected").val()
-							, location:$("select#location option:selected").val()
-							, os:$("input[name='os']:checked").val()
-							, character:$("#character").val()
-							, opinion:$("#opinion").val()
-						}
-						,type:"POST"
-						,success:function(e){
-							$("div#modalMsg").text(msg.success);
-							$("#bs-example-modal-lg").modal("toggle");
-						}
-						,error:function(){
-							$("div#modalMsg").text(msg.error);
-							$("#bs-example-modal-lg").modal("toggle");
-						}
-					});
-				});
+			$('#fullpage').fullpage({
+				verticalCentered: false
+				,scrollBar:true
 			});
+
+			$("#saveBtn").on("click",function(){
+				$("#sendForm").submit();
+			});
+
+			//vaildate
+			$("#sendForm").validate({
+				debug : true,
+				rules : {
+					 name : {required : true}
+					,mail : {required : true, email : true}
+				},
+				messages: {
+					name: {required: msg.emptyName}
+					,mail : {required: msg.emptyMail,email : msg.wrongMail}
+
+				},
+				// message : {
+				// 	 name : {required : msg.emptyName}
+				// 	,mail : {required : msg.emptyMail, email : msg.wrongMail}
+				// },
+
+				// errorPlacement:function(error,element){
+				// 	error.appendTo( element.closest("fieldset").next("div#errorMsg"));
+				// },
+
+				submitHandler: function() {
+					sendData();
+				},
+				errorClass:"validate_error"
+				,
+				validClass:"validate_valid"
+			});
+
+			//text size
+			$("h1#title").fitText(1,{ minFontSize: '50px', maxFontSize: '80px' });
+			$("h3#s2").fitText(2,{ minFontSize: '20px', maxFontSize: '40px' });
+			$("h3#s3").fitText(2,{ minFontSize: '20px', maxFontSize: '40px' });
+			$("h3#s4").fitText(2,{ minFontSize: '20px', maxFontSize: '40px' });
+			$("h1#s5").fitText(1,{ minFontSize: '40px', maxFontSize: '60px' });
+			$("span#s6").fitText(2,{ minFontSize: '30px', maxFontSize: '60px' });
+			$("h3#s6").fitText(1.8,{ minFontSize: '18px', maxFontSize: '40px' });
+			$("h3#s8").fitText(1,{ minFontSize: '20px', maxFontSize: '35px' });
+			$("h3#s9").fitText(2,{ minFontSize: '20px', maxFontSize: '20px' });
+		});	//document.ready End
+
+		function sendData(){
+			$.ajax({
+				 //url:"https://script.google.com/macros/s/AKfycbwow4e86Vh8Syq_1fzpct7STCHHpnFkU2etQUVhBE9wN9oqtpE/exec"//googledrive
+				 url:"https://listensoft.slack.com/services/hooks/slackbot?token=FnYtz5bBVRS7i3D7ahE51Yq6&channel=%23fb_teaser_writer"
+				,data:{name:$("#name").val()
+					, mail:$("#mail").val()
+				}
+				,type:"POST"
+				,success:function(e){
+					$("#name").val("");
+					$("#mail").val("");
+					$("div#modalMsg").text(msg.success);
+					$("#bs-example-modal-lg").modal("toggle");
+				}
+				,error:function(){
+					$("div#modalMsg").text(msg.error);
+					$("#bs-example-modal-lg").modal("toggle");
+				}
+			});
+
+		}
