71f5f36911dca88f8519e54fbd5b2318ba1bcbae
ted <ted@listensoft.net>
2017-06-28 14:52:22 +0900

    사전등록 바로가기 버튼수정

--- js/index.js
+++ js/index.js
@@ -51,7 +51,7 @@
 			$("span#s6").fitText(2,{ minFontSize: '30px', maxFontSize: '60px' });
 			$("h3#s6").fitText(1.8,{ minFontSize: '18px', maxFontSize: '40px' });
 			$("h3#s7").fitText(1,{ minFontSize: '20px', maxFontSize: '35px' });
-			$("span#moveBtn").fitText(1,{ minFontSize: '15px', maxFontSize: '30px' });
+
 
 		});	//document.ready End
 
