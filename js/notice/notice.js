b49281cf9d10f0c26376ec532208352a7745e8d9
okihouse <okihouse16@gmail.com>
2017-09-26 17:42:43 +0900

    - update notice js

--- js/notice/notice.js
+++ js/notice/notice.js
@@ -8,6 +8,7 @@ $(function() {
             this.data();
             this.cacheDom();
             this.bindEvent();
+            this.hash();
         },
         cacheDom: function(){
             this.$body = $('body');
@@ -29,17 +30,22 @@ $(function() {
         data: function(){
             $.getJSON('https://s3-ap-northeast-1.amazonaws.com/kyaraten-notice/data.json', function(data) {
                 $.each(data, function(index, value){
-                    var $template = notice.template();
+                    var $template = notice.template(index);
                     $template.find('.title').text(value.title)
                     $template.find('time').text(value.issueDate);
                     $template.find('article').html(value.content);
+
+                    var $anchor = $template.find('a.unit');
+                    if($anchor.attr('href') === notice.hash){
+                        $template.addClass('active');
+                    }
                     notice.$toggleList.append($template);
                 });
             });
         },
-        template: function(){
+        template: function(index){
             var $section = $('<section>' +
-                '<a href="#">' +
+                '<a href="#' + ++index +'" class="unit">' +
                 '<div class="titleWrapper">' +
                 '<div class="title"></div>' +
                 '<time></time>' +
