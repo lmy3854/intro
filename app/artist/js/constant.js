9f5207cf5c3c31d6facdc6914a676a3a82216ac4
ted <ted@listensoft.net>
2017-11-30 17:29:24 +0900

    notice lang param add(ja-JP, ko-KR)

---
+++ app/artist/js/constant.js
@@ -0,0 +1,38 @@
+lang_k = {
+	"tab_1":"공지사항",
+	"tab_2":"통계",
+	"tab_3":"정산",
+	"th_static_1":"제목",
+	"th_static_2":"썸네일",
+	"th_static_3":"가격",
+	"th_static_4":"view",
+	"th_static_5":"dw",
+	"th_static_6":"buy",
+	"th_static_7":"상태",
+	"th_account_1":"판매일",
+	"th_account_2":"판매스킨",
+	"th_account_3":"판매금액",
+	"th_account_4":"적용비율",
+	"th_account_5":"환급금액"
+}
+lang_k = {
+	"tab_1":"Notice",
+	"tab_2":"Static",
+	"tab_3":"Account",
+	"th_static_1":"Title",
+	"th_static_2":"Thumbnail",
+	"th_static_3":"Price",
+	"th_static_4":"View",
+	"th_static_5":"Down",
+	"th_static_6":"Buy",
+	"th_static_7":"State",
+	"th_account_1":"Date",
+	"th_account_2":"Skin",
+	"th_account_3":"Price",
+	"th_account_4":"Ratio",
+	"th_account_5":"Refund"
+}
+env_prd="https://api.kyaraten.com";
+env_dev="http://112.216.240.202:18888";
+s3_url="https://s3-ap-northeast-1.amazonaws.com";
+
