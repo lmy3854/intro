a17f920a682588784943cbe5b72f58b0a9b13db2
ted <ted@listensoft.net>
2017-11-30 17:38:12 +0900

    js file add

--- app/artist/js/artist.js
+++ app/artist/js/artist.js
@@ -1,39 +1,58 @@
 $(document).ready(function(){
-	getNoticeInfos();
-	$("#accountBtn").on("click",function(){
-		alert("In Ready!!!");
-		loading();
-	});
 	$("#myTab").find("a").on("click",function(){
 		var $this = $(this);
-		if($this.attr("href") == "#notice"){
-		}else if($this.attr("href") == "#static"){
-			$("#static").find("tbody").html("");
-			$("div#static").data("page","1");
-			getStaticInfos(1);
-		}else if($this.attr("href") == "#account"){
-			$("#account").find("tbody").html("");
-			$("div#account").data("page","1");
-			getAccountInfos(1);
-		}
+		selectTab($this);
 	});
 	$("button#staticMore").on("click",function(){
-		$("div#account").data("page","1");
 		var page = $("div#static").data("page");
 		getStaticInfos(page);
 	});
-
 	$("button#accountMore").on("click",function(){
 		var page = $("div#account").data("page");
 		getAccountInfos(page);
 	});
+	init();
 }); //document.ready End
+
+function selectTab($this){
+	if($this.attr("href") == "#static"){
+		$("#static").find("tbody").html("");
+		$("div#static").data("page","1");
+		getStaticInfos(1);
+	}else if($this.attr("href") == "#account"){
+		$("#account").find("tbody").html("");
+		$("div#account").data("page","1");
+		getAccountInfos(1);
+	}
+}
+
+function init(){
+	if(urlParam("lang")!=null){
+		$("#lang").val(urlParam("lang"));
+	}
+	if(urlParam("env")=="prd"){
+		$("#env").val(urlParam("prd"));
+	}
+	$("#appCode").val(urlParam("appcode"));
+	$("#accessToken").val(urlParam("accesstoken"));
+	$("#accountBtn").on("click",function(){
+		loading();
+		alert("In Ready!!!");
+		loading();
+	});
+	getNoticeInfos();
+}
 
 function getNoticeInfos(){
 	loading();
 	$("#accordion").html("");
-	//var endpoint = "https://s3-ap-northeast-1.amazonaws.com/kyaraten-notice/data-artist_kr.json";
-	var endpoint = "https://s3-ap-northeast-1.amazonaws.com/kyaraten-notice/data-artist.json";
+	var endpoint;
+	if($("#lang").val() == "ko-KR"){
+		endpoint = "https://s3-ap-northeast-1.amazonaws.com/kyaraten-notice/data-artist_kr.json";
+	}else{
+		endpoint = "https://s3-ap-northeast-1.amazonaws.com/kyaraten-notice/data-artist.json";
+	}
+
 	$.getJSON(endpoint, function(data) {
 		var i = 1;
 		$.each(data, function(index, value){
@@ -41,7 +60,7 @@ function getNoticeInfos(){
 			issueDate = value.issueDate;
 			content = value.content;
 
-			var $template = template(i);
+			var $template = notice_template(i);
 			$template.find("#title").text(title);
 			$template.find("#issueDate").text("["+issueDate+"]");
 
@@ -56,7 +75,7 @@ function getNoticeInfos(){
 	loading();
 }
 
-function template(i){
+function notice_template(i){
 	var $template =$(
 		'<div class="panel panel-default">'+
 			'<div class="panel-heading">'+
@@ -78,61 +97,69 @@ function template(i){
 
 function getStaticInfos(page){
 	if(page == undefined || page == null)page = 1;
-	var data = new Object();
-		data.appCode = "dcbce29f-9c5c-11e7-a341-6d284b271b6d-8PnYMk";
-		data.accessToken = "eyJhbGciOiJIUzUxMiJ9.eyJ1c2VyTm8iOjk3LCJhcHBDb2RlIjoiZGNiY2UyOWYtOWM1Yy0xMWU3LWEzNDEtNmQyODRiMjcxYjZkLThQbllNayIsImlzc3VlRGF0ZSI6MTUwNTczMDkyNDkwMn0.At3awhiO1J4uPxJJInF282UsI7k2OkAT7drlSLP3xQuQS0IJzmy42dXmjORStfkpq46Hclkk0Ilxjk9JZ1_bCw";
+	if($("#env").val() == "prd"){
+		apiUrl = "https://api.kyaraten.com/api/";
+	}else{
+		apiUrl = "http://112.216.240.202:18888/api/";
+	}
+	apiUrl += "v1/user/info/sales/"+page;
 	loading();
 	$.ajax({
 		type:"get",
-		url:"http://112.216.240.202:18888/api/v1/user/info/sales/"+page ,
+		url: apiUrl,
 		beforeSend : function(xhr){
 			xhr.setRequestHeader("Accept-Language",$("input#lang").val());
-			xhr.setRequestHeader("appCode",data.appCode);
-			xhr.setRequestHeader("accessToken",data.accessToken);
+			xhr.setRequestHeader("appCode",$("#appCode").val());
+			xhr.setRequestHeader("accessToken",$("#accessToken").val());
 		},
 		success : function (response){
-			loading();
 			if(response.code != 0){
 				alert(response.message);
+				loading();
 				return false;
 			}
 			var lastYn = response.last;
 			var data = response.innerContents;
 			createTable("static",data,lastYn);
+			loading();
 		},
 		error : function(e){
 			alert("error");
 			console.log(e);
 			loading();
 		}
-
 	});
 }
 
 function getAccountInfos(page){
-	var data = new Object();
-		data.appCode = "dcbce29f-9c5c-11e7-a341-6d284b271b6d-8PnYMk";
-		data.accessToken = "eyJhbGciOiJIUzUxMiJ9.eyJ1c2VyTm8iOjk3LCJhcHBDb2RlIjoiZGNiY2UyOWYtOWM1Yy0xMWU3LWEzNDEtNmQyODRiMjcxYjZkLThQbllNayIsImlzc3VlRGF0ZSI6MTUwNTczMDkyNDkwMn0.At3awhiO1J4uPxJJInF282UsI7k2OkAT7drlSLP3xQuQS0IJzmy42dXmjORStfkpq46Hclkk0Ilxjk9JZ1_bCw";
-	//test
-	if(page == undefined || page == null)page = 1;
 	loading();
+	if(page == undefined || page == null)page = 1;
+	var apiUrl;
+	if($("#env").val() == "prd"){
+		apiUrl = "https://api.kyaraten.com/api/";
+	}else{
+		apiUrl = "http://112.216.240.202:18888/api/";
+	}
+	apiUrl += "v1/user/sales/"+page;
+
 	$.ajax({
 		type:"get",
-		url:"http://112.216.240.202:18888/api/v1/user/sales/"+page,
+		url: apiUrl,
 		beforeSend : function(xhr){
 			xhr.setRequestHeader("Accept-Language",$("input#lang").val());
-			xhr.setRequestHeader("appCode",data.appCode);
-			xhr.setRequestHeader("accessToken",data.accessToken);
+			xhr.setRequestHeader("appCode",$("#appCode").val());
+			xhr.setRequestHeader("accessToken",$("#accessToken").val());
 		},
 		success : function (response){
-			loading();
 			if(response.code != 0){
 				alert(response.message);
+				loading();
 				return false;
 			}
 			var lastYn = response.last;
 			var data = response.innerContent;
 			createTable("account",data,lastYn);
+			loading();
 		},
 		error : function(e){
 			alert("error");
@@ -146,7 +173,7 @@ function getAccountInfos(page){
 function createTable(type, data, lastYn){
 	$.each(data, function(index, item){
 		var $tr = $("<tr>");
-		if(type=="static"){
+		if(type=="static"){""
 			var $img = $("<img>",{"src":item.image,"width":"100px"});
 			var buy = Number(item.price) * Number(item.downloadCount);
 			$tr.append($("<td>").text(item.name));
@@ -156,7 +183,6 @@ function createTable(type, data, lastYn){
 			$tr.append($("<td>").text(item.downloadCount));
 			$tr.append($("<td>").text(buy));
 			$tr.append($("<td>").text(item.status));
-
 		}else if(type=="account"){
 			var issueDate = new Date(item.issueDate).format("yyyy-MM-dd");
 			$tr.append($("<td>").text(issueDate));
@@ -170,6 +196,8 @@ function createTable(type, data, lastYn){
 	});
 	if(lastYn!="Y"){
 		$("button.btn-block").addClass("hide");
+	}else{
+		$("button.btn-block").removeClass("hide");
 	}
 	var page = Number($("div#"+type).data("page"));
 	$("div#"+type).data("page",page+1)
@@ -179,82 +207,3 @@ function loading(){
 	$("body").toggleClass("loading");
 	$("div.mask").toggleClass("hide");
 }
-
-/*
-$(function() {
-	'use strict';
-	var notice = {
-		env: {
-			endpoint: {
-				jp: 'https://s3-ap-northeast-1.amazonaws.com/kyaraten-notice/data.json',
-				ko: 'https://s3-ap-northeast-1.amazonaws.com/kyaraten-notice/data_kr.json'
-			},
-			accept_language: {
-				japan: 'ja',
-				korea: 'ko'
-			},
-			accept_languages: ['ja', 'ko']
-		},
-		init: function(){
-			this.data();
-			this.cacheDom();
-			this.bindEvent();
-		},
-		cacheDom: function(){
-			this.$body = $('body');
-			this.$el = $('.container');
-
-			this.$toggleList = this.$el.find('.toggleList');
-		},
-		bindEvent: function(){
-			notice.$toggleList.on('click', 'a', function(e){
-				e.preventDefault();
-				var isActive = $(this).parent().hasClass('active');
-				if (isActive){
-					$(this).parent().removeClass('active');
-				} else {
-					$(this).parent().addClass('active');
-				}
-			});
-		},
-		data: function(){
-			var endpoint = notice.endpoint();
-			$.getJSON(endpoint, function(data) {
-				$.each(data, function(index, value){
-					var $template = notice.template();
-					$template.find('.title').text(value.title)
-					$template.find('time').text(value.issueDate);
-					$template.find('article').html(value.content);
-					notice.$toggleList.append($template);
-				});
-			});
-		},
-		endpoint: function(){
-			var lang = url('?lang');
-			if(!(lang && notice.env.accept_languages.indexOf(lang) >= 0)) lang = undefined;
-			var country = lang || navigator.language || navigator.userLanguage;
-			switch (country) {
-				case notice.env.accept_language.japan:
-					return notice.env.endpoint.jp;
-				case notice.env.accept_language.korea:
-					return notice.env.endpoint.ko;
-				default:
-					return notice.env.endpoint.jp;
-			}
-		},
-		template: function(){
-			var $section = $('<section>' +
-				'<a href="#">' +
-				'<div class="titleWrapper">' +
-				'<div class="title"></div>' +
-				'<time></time>' +
-				'</div>' +
-				'</a>' +
-				'<article></article>' +
-				'</section>');
-			return $section;
-		}
-	}
-	notice.init();
-});
-*/
